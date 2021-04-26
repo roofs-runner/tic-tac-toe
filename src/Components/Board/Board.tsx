4349d2f560012a89e22ff8ce1c4f12d21ff27692
sergii.svitlookyi <bourneind@gmail.com>
2021-04-26 10:21:01 +0300

    Game

--- src/Components/Board/Board.tsx
+++ src/Components/Board/Board.tsx
@@ -1,61 +1,95 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Square from '../Square'
 import styled from 'styled-components'
 
-const BoardRow = styled.div`
+const GameWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
 
+const BoardRow = styled.div`
+  display: flex;
 `;
 
 const Status = styled.div`
   margin-bottom: 10px;
 `;
 
+// interface  BoardSquaresState {
+//   squares: (string | null)[]
+//   setSquares: () => void
+// }
+
 const Board = () => {
   // rows = 3;
   // cells = 3;
-  const status = 'Next player: X';
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setXNext] = useState(true);
+  const status = `Next player: ${isXNext ? 'X' : 'O'}`;
 
   const renderSquare = (i: number) => {
     return (
-      <Square />
+      <Square squareValue={squares[i]} onSquareClick={() => clickSquare(i)}/>
     )
   };
 
-    return (
-      <div>
-        <Status>{status}</Status>
-        <BoardRow>
-          {renderSquare(0)}
-          {renderSquare(1)}
-          {renderSquare(2)}
-        </BoardRow>
-        <BoardRow>
-          {renderSquare(3)}
-          {renderSquare(4)}
-          {renderSquare(5)}
-        </BoardRow>
-        <BoardRow>
-          {renderSquare(6)}
-          {renderSquare(7)}
-          {renderSquare(8)}
-        </BoardRow>
-      </div>
-    );
+  const clickSquare = (index: number) => {
+    if (calculateWinner(squares) || squares[index]) {
+      return;
+    }
+    console.log(squares);
+    const newSquares = squares.slice();
+    newSquares[index] = isXNext ? 'x' : 'o';
+    setXNext(!isXNext);
+    setSquares(newSquares);
   };
 
+  const calculateWinner = (squares: (string | null)[]) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
 
-  // private _renderRows = () => {
-  //   return Array.from({length: this.rows}).map((rows, index) =>
-  //     <TableRow key={index}>
-  //       {Array.from({length: this.cells}).map(cell => this._renderCells())}
-  //     </TableRow>);
-  //
-  // };
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
 
-  // private _renderCells = () => {
-  //   return (
-  //     <TableCell>{'O'}</TableCell>
-  //   )
-  // };
+  const winner = calculateWinner(squares);
+
+  return (
+    <GameWrapper>
+      <Status>{status}</Status>
+      {winner ? <div>{`The winner is ${winner}`}</div> : null}
+      <BoardRow>
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </BoardRow>
+      <BoardRow>
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </BoardRow>
+      <BoardRow>
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </BoardRow>
+    </GameWrapper>
+  );
+};
 
 export default Board
--- src/Components/Square/Square.tsx
+++ src/Components/Square/Square.tsx
@@ -3,19 +3,26 @@ import styled from 'styled-components'
 
 const SquareItem = styled.button`
   border: 1px solid #999;
-  font-size: 24px;
+  font-size: 30px;
   font-weight: bold;
-  line-height: 34px;
-  height: 34px;
-  padding: 0;
+  line-height: 1;
+  height: 60px;
+  max-height: 60px;
+  padding: 10px;
   text-align: center;
-  width: 34px;
+  width: 60px;
+  max-width: 60px;
 `;
 
-const Square = () => {
+interface SquareProps {
+  squareValue: string | null
+  onSquareClick: () => void
+}
+
+const Square = ({ squareValue, onSquareClick }: SquareProps) => {
   return (
-    <SquareItem>
-      Some button
+    <SquareItem onClick={onSquareClick}>
+      {squareValue}
     </SquareItem>
   )
 };
